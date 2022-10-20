9f435cfd87310d8db3129b13c9892216e5f237d3
Ashugirl <aviva.shuman@gmail.com>
2022-10-20 11:53:26 +0200

    dummy users made

---
+++ target/classes/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (name, email, password, userName, lastName, street, houseN, city, zip)
+VALUES ('Buffy', 'siezeTheDay@blabla.com', 'Ih8vamps', 'Slayer', 'Summers', 'Evergreen Terrace', 6, 'Sunnydale','90210');
+
+INSERT INTO users (name, email, password, userName, lastName, street, houseN, city, zip)
+VALUES ('Homer', 'yayDonuts@blabla.com', 'mmmmmmmm', 'DohMan', 'Simpson', 'Forest lane', 28, 'Springfield','90210');
+
+INSERT INTO users (name, email, password, userName, lastName, street, houseN, city, zip)
+VALUES ('Joe', 'JoeBlow@blabla.com', 'bladibla', 'JoeBlow', 'Smith', 'Main St', 6, 'Detroit','90210');
+

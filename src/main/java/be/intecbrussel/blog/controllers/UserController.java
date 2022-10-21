c95ca33f6acc881573272552043f33789fbcb401
sagaeva <sagaeva@gmail.com>
2022-10-21 12:47:56 +0200

    Added UserController en veranderen index.html en signup.html

---
+++ src/main/java/be/intecbrussel/blog/controllers/UserController.java
@@ -0,0 +1,70 @@
+package be.intecbrussel.blog.controllers;
+
+import be.intecbrussel.blog.data.User;
+import be.intecbrussel.blog.repositories.UserRepository;
+import be.intecbrussel.blog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/register")
+    public String userRegistrationPage(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "signup";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(Model model, @ModelAttribute("user") User user) {
+        model.addAttribute("user", userService.createUser(user));
+        return "redirect:/index";
+
+    }
+
+
+
+
+
+
+
+    @GetMapping("/index")
+    public String showAll(){
+
+
+        return "index";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
